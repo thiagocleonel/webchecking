f7169d8d3680801bc2df584d8d29fb19993b7ba0
Thiago Leonel <thiagocleonel@gmail.com>
2018-06-09 17:19:22 -0300

    Validation and finish page

--- app/controllers/experiment_controller.rb
+++ app/controllers/experiment_controller.rb
@@ -2,23 +2,31 @@ class ExperimentController < ApplicationController
 
   def index
     if(has_cookie)
-      @user = User.where(email: cookies.signed[:email]).first
-      redirect_to "#{experiment_finish_path}" if !@user.can_answer_more?
-      @poi_image = PointOfInterestImage.next
+        @user = User.where(email: cookies.signed[:email]).first
+        render "experiment/finish" if !@user.can_answer_more?
+        @poi_image = PointOfInterestImage.next
     else
       redirect_to "#{login_index_path}"
     end
   end
 
+
+
   def post
-    params["tags"].each do |point_of_interest_image_id,changes|
-      changes.each do |id,new_status|
-        a = Answer.new
-        a.user = User.where(email: cookies.signed[:email]).first
-        a.point_of_interest_image = PointOfInterestImage.where(id: point_of_interest_image_id).first
-        a.tag = Tag.where(id: id).first
-        a.answer = new_status
-        a.point_of_interest_image.increase.save if a.save
+    if(params["answer_id"])
+      #Editing
+
+    else
+      #Creating
+      params["tags"].each do |point_of_interest_image_id,changes|
+        changes.each do |id,new_status|
+          a = Answer.new
+          a.user = User.where(email: cookies.signed[:email]).first
+          a.point_of_interest_image = PointOfInterestImage.where(id: point_of_interest_image_id).first
+          a.tag = Tag.where(id: id).first
+          a.answer = new_status
+          a.point_of_interest_image.increase.save if a.save
+        end
       end
     end
     redirect_to "#{experiment_index_path}"
